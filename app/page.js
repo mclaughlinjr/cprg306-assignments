b0405f81e29c9c738b6a4e6dd4b111c39cd79911
mclaughlinjr <153246892+mclaughlinjr@users.noreply.github.com>
2024-10-22 22:38:46 -0600

    week 6

--- app/page.js
+++ app/page.js
@@ -10,6 +10,7 @@ export default function Page() {
           <li><Link href="/week-3">Week 3</Link></li>
           <li><Link href="/week-4">Week 4</Link></li>
           <li><Link href="/week-5">Week 5</Link></li>
+          <li><Link href="/week-6">Week 6</Link></li>
         </ul>
       </div>
       
---
+++ app/week-6/page.js
@@ -0,0 +1,16 @@
+import ItemList from "./item-list";
+
+
+
+export default function Page() {
+
+
+
+
+return (
+<ItemList />
+);
+}
+  
+
+
